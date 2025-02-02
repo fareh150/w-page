dd4c8b3ff25bf5e210772737d2cb922a249acf00
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2025-02-02 20:44:04 +0100

    timer set data to show

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import TimerComponent from './timer/timer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: TimerComponent
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  }
+];
