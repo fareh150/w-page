3612039eb21acf3406e10e169a1bf080cda45509
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2025-02-04 12:49:01 +0100

    use footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
     NavbarComponent,
+    FooterComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
@@ -26,4 +28,7 @@ export class AppComponent {
     text: 'Alba y Fareh',
     link: '',
   };
+
+  footerText = 'Alba y Fareh';
+  footerYear = 2026;
 }
