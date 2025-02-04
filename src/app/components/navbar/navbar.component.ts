9c413e5762e2165f10a43c131d0eebd635c253df
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2025-02-04 11:56:01 +0100

    addd navbar component

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,16 +1,38 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, signal } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ]
 })
-export class NavbarComponent {
-  @Input() tabs: { name: string; link: string; }[] = [];
-  @Input() logoUrl: string | undefined;
-  @Input() logoLink?: string;
+export class NavbarComponent implements OnInit {
+  open      = false;
+  logoUrl   = signal<string | null>(null);
+  logoText  = signal<string | null>(null);
+  logoLink  = signal<string | null>(null);
 
-  ngOnInit() {
-    console.log(`Number of tabs: ${this.tabs.length}`);
+  // Component Inputs
+  @Input() tabs: { name: string; link: string; }[] = [];
+  @Input() logoData: { url: string; text: string; link: string; } | undefined;
+
+  ngOnInit(): void {
+    this.checkLogoData();
+  }
+
+  checkLogoData() {
+    if (this.logoData) {
+      this.logoUrl.set(this.logoData.url && this.logoData.url.trim() !== '' ? this.logoData.url : null);
+      this.logoText.set(this.logoData.text && this.logoData.text.trim() !== '' ? this.logoData.text : null);
+      this.logoLink.set(this.logoData.link && this.logoData.link.trim() !== '' ? this.logoData.link : null);
+    }
+  }
+
+  toggleOpen() {
+    this.open = !this.open;
   }
 }
