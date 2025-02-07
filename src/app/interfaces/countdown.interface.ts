3bf0e4869da42f125467a29caf0083225cc25122
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2025-02-07 09:47:38 +0100

    wip

---
+++ src/app/interfaces/countdown.interface.ts
@@ -0,0 +1,14 @@
+export interface TimeElapsed {
+  years: number;
+  months: number;
+  days: number;
+  hours: number;
+  minutes: number;
+  seconds: number;
+}
+
+export interface YearsMonthsDaysElapsed {
+  years: number,
+  months: number,
+  days: number,
+}
